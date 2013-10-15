66048bd2e7d8ca6e3e05ebda2c9f82424bf9ed29
Shaozhen Ding <dsz0111@gmail.com>
2013-10-15 10:32:03 -0500

    Integration test with cloudfoundry

--- src/test/java/com/sample/mvc/IntegrationTest.java
+++ src/test/java/com/sample/mvc/IntegrationTest.java
@@ -1,13 +1,11 @@
 package com.sample.mvc;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import com.sample.domain.EmbededBean;
@@ -15,12 +13,16 @@ import com.sample.domain.MyBean;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
-@Ignore
 public class IntegrationTest {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	private final static String URL = "http://localhost:8080/showcase/rest";
+	private final String URL;
+	
+	
+	public IntegrationTest(){
+		URL = System.getProperty("integrationUrl") + "/rest";
+	}
 	
 	@Test
 	public void getRestTemplate(){
